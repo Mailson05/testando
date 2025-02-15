4be8217eec24d31f6053e7067c10237ae097bd93
Mailson <mailsonestudo48@gmail.com>
2025-02-14 22:25:57 -0300

    adicionando idade

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        int idade = 10;
+
         System.out.println("Testando comandos git");
         System.out.println("Mais atualização");
         System.out.println("ultimo teste");
+
 
     }
 }
